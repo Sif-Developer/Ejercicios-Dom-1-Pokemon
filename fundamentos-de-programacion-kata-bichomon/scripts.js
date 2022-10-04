64cd2cd1f45f00031e24205db29f28346c523f81
Sif Et tabaa <sifbusinesswork@gmail.com>
2022-10-04 08:43:22 +0200

    Ejercicios Dom

---
+++ fundamentos-de-programacion-kata-bichomon/scripts.js
@@ -0,0 +1,30 @@
+let generasion1 = document.getElementById("gen-1");
+generasion1.innerHTML = "Generasión 1 Pokimon"
+
+let cambiaColor = document.querySelector(".infocard-list-pkmn-lg");
+cambiaColor.style.background = "green";
+console.log(cambiaColor)
+
+let myURL;
+myURL = document.URL;
+
+console.log(myURL);
+
+let myDomain;
+myDomain = document.domain;
+
+console.log(myDomain)
+
+
+// document.getElementsByClassName("img-fixed img-sprite").src = 'https://media.giphy.com/media/2v170e71aanfi/giphy.gif';
+
+// let cambiaImagen = document.querySelector(".img-fixed img-sprite");
+// cambiaImagen.src ="https://media.giphy.com/media/2v170e71aanfi/giphy.gif"
+// console.log(cambiaImagen)
+
+
+//(¿?)No reconoce el src... He probado a poner a llamar al padre div y no "img-fixed img-sprite" que está en la misma linea pero sigue sin funcionar...
+// Cuando cambio el src desde la consola de manera manual si funciona            //
+
+
+
